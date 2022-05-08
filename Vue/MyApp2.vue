405cff71fdab5f65bf9fc345a912b19330c1d5e1
moonjubilees <100246752+moonjubilees@users.noreply.github.com>
2022-05-08 22:33:18 +0900

    Vue를 이용한 기본 예제

---
+++ Vue/MyApp2.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>
+            <ul> 
+                <li v-for="(m , i) in menus" v-bind:key="m" > <div v-if="m == 'board'" >{{i+1}}번째 메뉴 =  {{m}} </div> </li>
+
+            </ul>
+            
+        </h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name : "MyApp2" , 
+    data() {
+        return {
+            menus : ["home" , "boardlist" , "board"]
+        }//리턴 객체내부변수들
+    }//함수
+
+}
+
+</script>
+<style>
+h1  {color : green;}
+</style>
---
+++ Vue/components/BoardRow.vue
@@ -0,0 +1,26 @@
+<template>
+     <tbody>
+         <tr v-for="(oneboard) in board" :key="oneboard.seq" >
+             <td>{{oneboard.seq}}</td><td>{{oneboard.title}}</td><td>{{oneboard.writer}}</td><td>{{oneboard.viewcount}}</td>
+             <td><button @click="deleteHandler" :id="oneboard.seq"> 삭제 </button></td>
+             <td><button @click="updateHandler" :id="oneboard.seq"> 수정 </button></td>
+         </tr>
+     </tbody>
+</template>
+<script>
+
+export default {
+    name:"BoardRow" , 
+    props :{
+        board : {type:Array}
+    } , 
+    methods : {
+        deleteHandler (e) {   console.log("삭제:"+e.target.id); this.$emit("deleteFromChild", e.target.id ); } , 
+        updateHandler (e) { console.log("수정:"+e.target.id); this.$emit("updateFromChild" , {seq : e.target.id , title: '수정한 제목', writer : '수정한 작성자', viewcount : 0 }  ) ; }
+    }    
+
+}
+</script>
+<style>
+td { border : 2px solid silver; width : 100px; text-align:center ; }
+</style>
