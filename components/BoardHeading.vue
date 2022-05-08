24406a4f54bf38dfd8c7928526a27c8e74bf47df
moonjubilees <100246752+moonjubilees@users.noreply.github.com>
2022-05-08 22:30:03 +0900

    Vue를 이용한 기본 예제
    게시판 CRUD 기능 구현
    Vue를 이용해 화면단 구성하기

---
+++ components/BoardHeading.vue
@@ -0,0 +1,22 @@
+<template>
+     <thead><tr><th>{{seq}}</th><th>{{title}}</th><th>{{writer}}</th><th>{{viewcount}}</th><th colSpan="2">{{action}}</th></tr></thead>
+</template>
+<script>
+
+export default {
+    name:"BoardHeading" , 
+    data(){
+        return {
+           seq :"번호",
+           title :"제목",
+           writer :"작성자",
+           viewcount : "조회수",
+           action : '추가동작'
+        }
+    },
+
+}
+</script>
+<style>
+th { border : 2px solid silver; width : 100px; text-align:center ; background-color:rgb(34, 163, 163);}
+</style>
