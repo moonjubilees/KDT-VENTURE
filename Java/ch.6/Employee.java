7b812cfcf9ae2e95412fefdee44f80abfb0f00ea
moonjubilees <100246752+moonjubilees@users.noreply.github.com>
2022-06-20 14:51:18 +0900

    Create Employee.java

---
+++ Java/ch.6/Employee.java
@@ -0,0 +1,18 @@
+package ch6;
+//회사원들의 정적- 사원 이름 부서 급여 / 동적인 특성을 추출 필요 - 일하다 - 실행 목적 클니다 
+/*배열, 클래스에서 정적 변수 선언을 할 때,  자동으로 들어갈 수 있는 값이 정해져있음 
+ * int 정수타입 = 자동 0
+ * double 실수타입 = 자동 0.0
+ * boolean - false
+ * char - '\u0000' -유니코드 16진수 0000
+ * 모든 참조형 - 주소가 다 가진 것, 주소가 없다는 것은 null 
+ * */
+public class Employee {
+    //정적 - 변수
+	//동적 - 기능
+	int id =1000;
+	String name ;
+	String dept;
+	double pay;
+	
+}
