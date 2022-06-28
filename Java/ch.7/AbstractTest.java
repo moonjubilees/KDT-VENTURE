20cd587bf7e95575ab3e35d672d885dcc0b18acf
moonjubilees <100246752+moonjubilees@users.noreply.github.com>
2022-06-28 13:52:10 +0900

    Create AbstractTest.java

---
+++ Java/ch.7/AbstractTest.java
@@ -0,0 +1,24 @@
+package ch7;
+
+abstract class Shape{
+	abstract public void area();
+	abstract public void circum();
+}
+
+class Rectangle extends Shape{
+	int width = 0, height = 0;
+	
+	public void area() {
+		
+	}
+	
+	
+	public void circum() {}
+}
+public class AbstractTest {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
