d106bae6f52c90ba7eb56538e32f6c4851d61e70
moonjubilees <100246752+moonjubilees@users.noreply.github.com>
2022-07-05 22:43:22 +0900

    Create StudentTest.java

---
+++ Java/ch.7/StudentTest.java
@@ -0,0 +1,8 @@
+package ch7;
+
+public interface Student {
+	   /*public static final 생략된 것*/String name = "학생"; //변수값 필요하면 할당해야함
+	   /*abstract public 생략된 것*/void study();
+	   void lunch();
+	   
+	}
